4cb5fa3f1194f08e3050f0b085fcdfd5f7b97db5
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-01-22 17:05:07 +0300

    icons added to symbols type

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,7 +13,8 @@ const MAPPING = {
   "paperplane.fill": "send",
   "chevron.left.forwardslash.chevron.right": "code",
   "chevron.right": "chevron-right",
-  "list.fill": "list",
+  magnifyingglass: "search",
+  "paintbrush.fill": "brush",
 } as Partial<
   Record<
     import("expo-symbols").SymbolViewProps["name"],
