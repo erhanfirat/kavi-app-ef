5020de5aa04414e0d1924431ea42e6823aed57e5
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-01-22 14:32:51 +0300

    link added to post item

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -2,24 +2,30 @@ import { Post } from "@/api/types";
 import { ThemedText } from "./ThemedText";
 import { ThemedView } from "./ThemedView";
 import { StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export const PostItem = ({ post }: { post: Post }) => {
   return (
     <ThemedView style={styles.postContainer}>
-      <ThemedText>
-        {post.title} - {post.body}
+      <ThemedText type="subtitle" style={styles.postTitle}>
+        {post.title}
       </ThemedText>
+      <ThemedText>{post.body}</ThemedText>
+      <Link href={`/posts/${post.id}`}>Detail</Link>
     </ThemedView>
   );
 };
 
 const styles = StyleSheet.create({
   postContainer: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: "column",
+    alignItems: "flex-start",
     gap: 8,
     borderRadius: 8,
     borderWidth: 1,
     padding: 8,
+  },
+  postTitle: {
+    flex: 1,
   },
 });
