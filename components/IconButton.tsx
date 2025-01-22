73f353e828804c551401af00b7bd4f3027e61ece
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-01-22 17:03:55 +0300

    icon button component created

---
+++ components/IconButton.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { IconSymbol } from "./ui/IconSymbol";
+
+type IconButtonProps = {
+  title?: string;
+  icon?: import("expo-symbols").SymbolViewProps["name"];
+  onPress: () => {};
+};
+
+const IconButton: React.FC<IconButtonProps> = ({ icon, title, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      {icon && <IconSymbol size={28} name={icon} color={styles.text.color} />}
+      {title && <Text style={styles.text}>{title}</Text>}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#007BFF",
+    padding: 10,
+    borderRadius: 5,
+  },
+  text: {
+    color: "#fff",
+    marginLeft: 5,
+    fontSize: 16,
+  },
+});
+
+export default IconButton;
