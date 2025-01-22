67ffb561728e4ba2a3d6853ef770dde1764e0950
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-01-22 11:37:47 +0300

    api comments

--- api/api.ts
+++ api/api.ts
@@ -1,3 +1,7 @@
+/**
+ * Fetchs Posts
+ * @returns {Post[]}
+ */
 export const fetchPosts = async () => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   if (!res.ok) {
@@ -7,6 +11,11 @@ export const fetchPosts = async () => {
   return res.json();
 };
 
+/**
+ * Fetchs Post details by post id
+ * @param postId 
+ * @returns {Post}
+ */
 export const fetchPostById = async (postId: number) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${postId}`
@@ -18,6 +27,11 @@ export const fetchPostById = async (postId: number) => {
   return res.json();
 };
 
+/**
+ * Fetchs User info by userId 
+ * @param userId 
+ * @returns user
+ */
 export const fetchUserById = async (userId: number) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/users/${userId}`
