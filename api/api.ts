ec85c6ca403b2a471e85a9242e6e54db2fd9bbe1
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-01-22 11:59:32 +0300

    api types implemented

--- api/api.ts
+++ api/api.ts
@@ -1,8 +1,10 @@
+import { Post, User } from "./types";
+
 /**
  * Fetchs Posts
- * @returns {Post[]}
+ * @returns {Post[]} Post list
  */
-export const fetchPosts = async () => {
+export const fetchPosts = async (): Promise<Post[]> => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   if (!res.ok) {
     throw new Error("Veri çekme hatası");
@@ -13,10 +15,10 @@ export const fetchPosts = async () => {
 
 /**
  * Fetchs Post details by post id
- * @param postId 
- * @returns {Post}
+ * @param postId
+ * @returns {Post} Post information
  */
-export const fetchPostById = async (postId: number) => {
+export const fetchPostById = async (postId: number): Promise<Post> => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${postId}`
   );
@@ -28,11 +30,11 @@ export const fetchPostById = async (postId: number) => {
 };
 
 /**
- * Fetchs User info by userId 
- * @param userId 
- * @returns user
+ * Fetchs User info by userId
+ * @param userId
+ * @returns {User} User information
  */
-export const fetchUserById = async (userId: number) => {
+export const fetchUserById = async (userId: number): Promise<User> => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/users/${userId}`
   );
