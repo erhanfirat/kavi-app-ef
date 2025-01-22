3f12eab934779f904535792c105b486db4676c5c
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-01-22 17:03:14 +0300

    auto navigate back on not found

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,18 +1,28 @@
-import React from "react";
-import { Link, Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { Link, router, Stack } from "expo-router";
 import { StyleSheet } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function NotFoundScreen() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/posts"); // Yönlendirmek istediğiniz rota
+    }, 5000); // 3000 ms = 3 saniye
+
+    return () => clearTimeout(timer); // Temizleme işlemi
+  }, []);
+
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
       <ThemedView style={styles.container}>
         <ThemedText type="title">This screen doesn't exist.</ThemedText>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <ThemedText type="link">
+            You will be redirected to Home page in 5 seconds or click here!
+          </ThemedText>
         </Link>
       </ThemedView>
     </>
